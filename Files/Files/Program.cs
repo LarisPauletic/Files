13662d47a9ecbacdb76d2873eb0b037cad723a93
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-04-06 14:34:44 +0200

    Add files via upload

---
+++ Files/Files/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace FileKlasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite Vaše ime: ");
+            string ime = Console.ReadLine();
+            string datoteka = "ime.txt";
+            
+            if (File.Exists(datoteka))
+            {
+                
+                if (!Directory.Exists("backup"))
+                {
+                    Directory.CreateDirectory("backup");
+                }
+                File.Copy(datoteka, "backup\\ime_" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss") + ".txt");
+               
+                File.Delete(datoteka);
+            }
+           
+            File.WriteAllText(datoteka, ime);
+        }
+    }
+}
